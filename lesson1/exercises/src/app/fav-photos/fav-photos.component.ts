eb7b7ed3a2f518b09b559f4dd44b3003883648dd
Courtney Phillips <quirkyfiddie@yahoo.com>
2020-12-14 22:38:42 -0600

    Angular 28.7.3. Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Miscellaneous Corgis';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.dog.ceo/breeds/corgi-cardigan/n02113186_1612.jpg';
+  image3 = 'https://images.dog.ceo/breeds/corgi-cardigan/n02113186_2994.jpg';
 
   constructor() { }
 
